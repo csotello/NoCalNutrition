23a2ed7957a5502d5b5f6438da74f61a132adfa5
Carlos Sotello <59632116+csotello@users.noreply.github.com>
2022-11-03 16:40:45 -0700

    Add support for persistent storage

--- App.js
+++ App.js
@@ -1,32 +1,65 @@
-import React,{useState} from 'react';
+import React,{useState,useRef, useEffect} from 'react';
 import {
   ScrollView,
   StyleSheet,
   Text,
-  useColorScheme,
   View,
   Keyboard,
   TextInput,
   Button,
   Dimensions,
 } from 'react-native';
+import EncryptedStorage from 'react-native-encrypted-storage'
 import Task from './components/Task'
+
 const width = Dimensions.get('window').width;
 const height = Dimensions.get('window').height;
 const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-  const [tasks,setTask] = useState(["w"]);
+  const [tasks,setTask] = useState([]);
   const [text,setText] = useState('');
+  const [loaded,setLoaded] = useState(false)
+  const load = async () => {
+    try {
+      const val = await EncryptedStorage.getItem("tasks")
+      if(val){
+        setTask(JSON.parse(val))
+        console.log("Loaded")
+      }
+    } catch (error) {
+      console.log(error)
+      console.log("Failed to load")
+    }
+  }
+  if(!loaded){
+    load()
+    setLoaded(true)
+  }
+  const store = async (tasks)=>{
+    try {
+      await EncryptedStorage.setItem(
+        "tasks",
+        JSON.stringify(tasks)
+      )
+      console.log("Stored:" + JSON.stringify(tasks))
+    } catch (error) {
+      console.log("Failed to store")
+    }
+  }
   const add = (task) =>{
     if (task == null) return;
+    var current = [...tasks,task]
+    store(current)
     setTask([...tasks,task]);
-    setText("")
+    setText("");
     Keyboard.dismiss();
   }
   const remove = (task) =>{
-    setTask(tasks.filter((val,i) =>
-      val != task
-    ))
+    setText("");
+    var current = tasks.filter((val,i) =>
+    val != task
+  )
+    store(current)
+    setTask(current)
   } 
   return (
     <View style={styles.background}>
@@ -49,18 +82,19 @@ const App = () => {
        <TextInput 
           onChangeText={txt => setText(txt)}
           style={{height:50}}
-          placeholder='text'>
+          placeholder='text'
+          value={text}>
 
         </TextInput>
         <Button 
           title="Add"
           color='#2db92d'
-          onPress={(txt) => add(text)}
+          onPress={() => add(text)}
           style={styles.button}></Button>
         <Button 
           title="Remove"
           color='#2db92d'
-          onPress={(txt) => remove(text)}
+          onPress={() => remove(text)}
           style={styles.button}></Button>
       </View>
     </View>
