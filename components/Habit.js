d0bd2d45ca08ea1db355e5ae83bb8064f57e38e4
Carlos Sotello <59632116+csotello@users.noreply.github.com>
2022-11-04 21:42:05 -0700

    Update Habits

--- components/Habit.js
+++ components/Habit.js
@@ -1,9 +1,10 @@
 import { View, Text } from "react-native"
-import {Box, Button, Flex} from 'native-base'
-import { Icon } from "react-native-vector-icons/EvilIcons"
+import {Box, Button, Flex, IconButton} from 'native-base'
+import Icon  from "react-native-vector-icons/EvilIcons"
 import { useState } from "react"
 const Habit = (props) => {
     const [days,setDays] = useState(props.days)
+    const dayText = {Monday:"M",Tuesday:"T",Wednesday:"W",Thursday:"Th",Friday:"F",Saturday:"S",Sunday:"Su"}
     const handleClick = (day) => {
         let cur = days[day]
         days[day] = cur == "Default" ? "Complete" : "Default"
@@ -13,11 +14,20 @@ const Habit = (props) => {
     }
     return(
         <View>
-            <Box>
-                <Text>{props.title}</Text>
+            <Box rounded={"lg"}>
+            <Flex direction="row" w={"100%"}>
+                <Text style={{paddingLeft:20}}>{props.title}</Text>
+                <IconButton
+                    style={{paddingLeft:50}}
+                    right={-100}
+                    top={-10}
+                    icon={<Icon name="trash" size={30}/>}
+                    onPress={() => props.remove(props.title)}
+                    />
+                </Flex>
                 <Flex direction="row" w={"100%"}>
                 {Object.keys(days).map((day,i) => {
-                    return <Button w={"14%"} h={10} key={i} onPress={(evt) => handleClick(day)} background={days[day] == "Default" ? "#6fdc6f" : "#178237"}>{day}</Button>
+                    return <Button rounded borderRadius={100} variant="outline" borderColor={"amber.100"} w={"14%"} h={10} key={i} onPress={(evt) => handleClick(day)} background={days[day] == "Default" ? "#6fdc6f" : "#178237"}>{dayText[day]}</Button>
                 })}
                 </Flex>
             </Box>
--- screens/Habits.js
+++ screens/Habits.js
@@ -1,8 +1,14 @@
 import { useState } from "react"
-import { ScrollView, View, Text } from "react-native"
+import { ScrollView, View, Text, Keyboard, ToastAndroid } from "react-native"
 import {Box} from "native-base"
 import Habit from "../components/Habit"
-const Habits = () => {
+import { IconButton, Modal, Input, Button } from "native-base"
+import  Icon  from "react-native-vector-icons/FontAwesome5"
+import EncryptedStorage from 'react-native-encrypted-storage'
+const Habits = ({route}) => {
+    const [visible,setVisible] = useState(false)
+    const [text,setText] = useState("")
+    const [loaded,setLoaded] = useState(false)
     const [habits,setHabits] = useState([{
         title:"Drink 100oz of Water",
         days:{
@@ -15,15 +21,98 @@ const Habits = () => {
             Sunday:"Default",
         }
     }])
+    const load = async () => {
+        try {
+          const val = await EncryptedStorage.getItem("habits")
+          if(val){
+            setHabits(JSON.parse(val))
+            console.log("Loaded")
+          }
+        } catch (error) {
+          console.log(error)
+          console.log("Failed to load")
+        }
+    }
+    if(!loaded){
+        load()
+        setLoaded(true)
+    }
+    const store = async (habits)=>{
+        try {
+          await EncryptedStorage.setItem(
+            "habits",
+            JSON.stringify(habits)
+          )
+          console.log("Stored:" + JSON.stringify(habits))
+        } catch (error) {
+          console.log("Failed to store")
+        }
+    }
+    const add = (habit) =>{
+        if(!habit) return;
+        console.log(habit.title)
+        let cur = {
+            title:habit,
+            days:{
+                Monday:"Default",
+                Tuesday:"Default",
+                Wednesday:"Default",
+                Thursday:"Default",
+                Friday:"Default",
+                Saturday:"Default",
+                Sunday:"Default",
+            }
+        }
+        let has = false
+        habits.forEach(habit=>{
+            if(habit.title == cur.title)
+                has = true
+        })
+        if(!has){
+            store([...habits,cur])
+            setHabits([...habits,cur])
+        }
+        else{
+            ToastAndroid.show("Habit already exists",ToastAndroid.TOP)
+        }
+        setText("")
+        setVisible(false)
+        Keyboard.dismiss()
+    }
+    
+    const remove = (title) =>{
+        console.log(title)
+        let cur = habits.filter(x => x.title != title)
+        store(cur)
+        setHabits(cur)
+    }
     return(
         <View>
             <Box>
-                <Text>Habits</Text>
+                <Text style={{fontSize:20,alignSelf:"center", paddingBottom:10}}>Habits</Text>
                 <ScrollView>
                     {habits.map((habit,i) => {
-                        return <Habit title={habit.title} days={habit.days} key={i}></Habit>
+                        return <Habit title={habit.title} days={habit.days} key={i} remove={remove}></Habit>
                     })}
                 </ScrollView>
+                <Modal isOpen={visible} onClose={() => setVisible(prev => !prev)} size="lg">
+                    <Modal.Content h={"80%"} background="#6fdc6f" paddingTop={10}>
+                        <Modal.CloseButton/>
+                        <Text style={{fontSize:20}}>New Habit</Text>
+                        <Input value={text} onChangeText={(txt) => setText(txt)} placeholder={"habit"}></Input>
+                        <Button onPress={() => add(text)}>Add</Button>
+                    </Modal.Content>
+                </Modal>
+                <IconButton
+                    size={10}
+                    rounded="100"
+                    variant="solid"
+                    position={"absolute"}
+                    icon={<Icon name="plus" size={20}/>}
+                    onPress={() => setVisible(prev => !prev)}
+                    right={15}
+                    top={450}
+                    />
             </Box>
         </View>
     )
