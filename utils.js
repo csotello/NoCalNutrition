771e7b99c7180fd1b1ed8abfe36b1f821d2940b1
Carlos Sotello <59632116+csotello@users.noreply.github.com>
2023-02-11 22:05:08 -0800

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,52 +1,57 @@
 //Convert user input into format used by API
 export const convertCustomFood = food => {
   ret = {
-    description: food.foodName,
-    householdServingFullText: food.serving,
-    servingSize: food.servingWeight,
-    servingSizeUnit: `${food.servingUnit}`.toLocaleUpperCase(),
+    description: food.foodName || '',
+    householdServingFullText: food.serving || '',
+    servingSize: food.servingWeight || '',
+    servingSizeUnit: `${food.servingUnit || ''}`.toLocaleUpperCase(),
     foodNutrients: [
       {
         nutrientName: 'Protein',
-        value: food.protein,
+        value: food.protein || 0,
         unitName: 'G',
       },
       {
         nutrientName: 'Total Lipid (fat)',
-        value: food.totalFat,
+        value: food.totalFat || 0,
         unitName: 'G',
       },
       {
         nutrientName: 'Carbohydrate, by difference',
-        value: food.carbs,
+        value: food.carbs || 0,
         unitName: 'G',
       },
       {
         nutrientName: 'Sugars, total including NLEA',
-        value: food.sugar,
+        value: food.sugar || 0,
         unitName: 'G',
       },
       {
         nutrientName: 'Fiber, total dietary',
-        value: food.fiber,
+        value: food.fiber || 0,
         unitName: 'G',
       },
       {
         nutrientName: 'Sodium, Na',
-        value: food.sodium,
+        value: food.sodium || 0,
         unitName: 'MG',
       },
       {
         nutrientName: 'Fatty acids, total saturated',
-        value: food.saturatedFat,
+        value: food.saturatedFat || 0,
         unitName: 'G',
       },
       {
         nutrientName: 'Cholesterol',
-        value: food.cholesterol,
+        value: food.cholesterol || 0,
         unitName: 'MG',
       },
     ],
   };
   return ret;
+};
+
+export const defaultFood = {
+  name: '',
+  description: '',
 };
