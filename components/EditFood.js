b50356a12e54bd2f9e695b7db9d0019b4c242376
Carlos Sotello <59632116+csotello@users.noreply.github.com>
2023-03-12 21:01:19 -0700

    Update EditFood.js

--- components/EditFood.js
+++ components/EditFood.js
@@ -1,31 +1,83 @@
+import {Flex, Input} from 'native-base';
+import {useEffect, useState} from 'react';
 import {View, Text} from 'react-native';
 
 const EditFood = props => {
-  console.log(props.food);
-  console.log('Edit page');
+  const [servings, setServings] = useState(0);
+  const [nutrients, setNutrients] = useState({
+    protein: 0,
+    carbs: 0,
+    fat: 0,
+  });
+  useEffect(() => {
+    const serving = Number(props.food.servingSize).toPrecision(4);
+    setServings(serving || 0);
+    var cur = {...nutrients};
+    props.food.foodNutrients.map((nutrient, i) => {
+      switch (nutrient.nutrientName) {
+        case 'Protein':
+          cur.protein = nutrient.value;
+          break;
+        case 'Carbohydrate, by difference':
+          cur.carbs = nutrient.value;
+          break;
+        case 'Total lipid (fat)':
+          cur.fat = nutrient.value;
+          break;
+        default:
+          break;
+      }
+    });
+    setNutrients({...cur});
+  }, [props.food]);
   return (
-    <View style={{zIndex: 99}}>
-      <Text>Edit</Text>
+    <View>
       <Text>{props.food.description}</Text>
-      {props.food.foodNutrients.map((nutrient, i) => {
-        if (
-          nutrient.nutrientName.match(
-            /^(Protein|Carbohydrate, by difference|Total lipid \(fat\))$/,
-          )
-        ) {
-          return (
-            <>
-              <View style={{paddingLeft: 5}} key={i}>
-                <Text>
-                  {nutrient.nutrientName} {nutrient.value} {nutrient.unitName}{' '}
-                </Text>
-              </View>
-            </>
-          );
-        } else {
-          return <></>;
-        }
-      })}
+      <Flex direction="row" style={{marginBottom: 10}}>
+        <Text style={{paddingTop: 10, paddingRight: 10}}>Servings:</Text>
+        <Input
+          value={servings.toString()}
+          onChangeText={text => setServings(Number(text))}
+          w={100}
+          h={10}
+        />
+      </Flex>
+      <Flex direction="row">
+        <Text>Protein: </Text>
+        <Input
+          value={nutrients.protein.toString()}
+          onChangeText={text =>
+            setNutrients({...nutrients, protein: parseFloat(text) || 0})
+          }
+          w={100}
+          h={10}
+        />
+        <Text>g</Text>
+      </Flex>
+      <Flex direction="row">
+        <Text>Carbs: </Text>
+        <Input
+          value={nutrients.carbs.toString()}
+          onChangeText={text =>
+            setNutrients({...nutrients, carbs: parseFloat(text)})
+          }
+          w={100}
+          h={10}
+        />
+        <Text>g</Text>
+      </Flex>
+      <Flex direction="row">
+        <Text>Fat: </Text>
+        <Input
+          value={nutrients.fat.toString()}
+          onChangeText={text =>
+            setNutrients({...nutrients, fat: parseFloat(text)})
+          }
+          w={100}
+          h={10}
+        />
+        <Text>g</Text>
+      </Flex>
     </View>
   );
 };
