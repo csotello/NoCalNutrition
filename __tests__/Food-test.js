155e9d9cdecd6a42c53d9163cff050faf11b6b4a
Carlos Sotello <59632116+csotello@users.noreply.github.com>
2023-03-30 19:58:04 -0700

    Update Food-test.js

--- __tests__/Food-test.js
+++ __tests__/Food-test.js
@@ -1,11 +1,49 @@
-import {render, screen, fireEvent} from '@testing-library/react-native';
+import {
+  render,
+  screen,
+  cleanup,
+  fireEvent,
+} from '@testing-library/react-native';
 import Food from '../components/Food';
 import {NativeBaseProvider} from 'native-base';
+import {defaultFood} from '../utils';
+
+afterEach(cleanup);
+const inset = {
+  frame: {x: 0, y: 0, width: 0, height: 0},
+  insets: {top: 0, left: 0, right: 0, bottom: 0},
+};
 
 it('renders', () => {
   render(
-    <NativeBaseProvider>
+    <NativeBaseProvider initialWindowMetrics={inset}>
       <Food />
     </NativeBaseProvider>,
   );
+});
+
+it('Has functional Icons', () => {
+  let edit = jest.fn();
+  let remove = jest.fn();
+
+  render(
+    <NativeBaseProvider initialWindowMetrics={inset}>
+      <Food
+        edit={edit}
+        remove={remove}
+        meal={[{...defaultFood}]}
+        title="Breakfast"
+      />
+    </NativeBaseProvider>,
+  );
+
+  const deleteIcon = screen.queryAllByLabelText('Delete Icon');
+  const editIcon = screen.queryAllByLabelText('Edit Icon');
+
+  fireEvent.press(editIcon[0]);
+  fireEvent.press(deleteIcon[0]);
+  const deleteButton = screen.getByLabelText('Delete Button');
+  fireEvent.press(deleteButton);
+  expect(remove).toHaveBeenCalled();
+  expect(edit).toHaveBeenCalled();
 });
