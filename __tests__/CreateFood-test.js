c5420aa0072de31b2e4ca43bab240ba1c94b244d
Carlos Sotello <59632116+csotello@users.noreply.github.com>
2023-03-30 19:58:07 -0700

    Update CreateFood-test.js

--- __tests__/CreateFood-test.js
+++ __tests__/CreateFood-test.js
@@ -1,11 +1,33 @@
-import {render, screen, fireEvent} from '@testing-library/react-native';
+import {
+  render,
+  screen,
+  fireEvent,
+  cleanup,
+} from '@testing-library/react-native';
 import CreateFood from '../components/CreateFood';
 import {NativeBaseProvider} from 'native-base';
-
+afterEach(cleanup);
+const inset = {
+  frame: {x: 0, y: 0, width: 0, height: 0},
+  insets: {top: 0, left: 0, right: 0, bottom: 0},
+};
 it('renders', () => {
   render(
     <NativeBaseProvider>
       <CreateFood />
     </NativeBaseProvider>,
   );
+});
+
+it('Handles user input', () => {
+  render(
+    <NativeBaseProvider initialWindowMetrics={inset}>
+      <CreateFood isNew={true} food={{}} />
+    </NativeBaseProvider>,
+  );
+  const brandName = screen.getByPlaceholderText('Kroger');
+  fireEvent.changeText(brandName, 'Kroger');
+  expect(brandName.props.value).toBe('Kroger');
+  const button = screen.getByText('Create');
+  fireEvent.press(button);
 });
