96c2a4b0056de486039d7326d84a9450001348a4
Carlos Sotello <59632116+csotello@users.noreply.github.com>
2023-04-09 19:51:41 -0700

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = async () => {
   return {
     preset: 'react-native',
-    setupFiles: ['<rootDir>/tests/setup.js'],
+    setupFilesAfterEnv: ['<rootDir>/tests/setup.js'],
     transformIgnorePatterns: [
       'node_modules/(?!(@react-native|native-base|react-native|react-native-vector-icons|react-navigation|@react-navigation|react-native-paper|react-native-iphone-x-helper)/)',
     ],
