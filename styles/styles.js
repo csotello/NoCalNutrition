721845b323a4e390e3a0480334d9d806ce282d36
Carlos Sotello <59632116+csotello@users.noreply.github.com>
2023-05-06 18:08:20 -0700

    Update styles.js

--- styles/styles.js
+++ styles/styles.js
@@ -1,6 +1,23 @@
 import {StyleSheet} from 'react-native';
 
 const styles = StyleSheet.create({
+  primaryBackgroundColor: '#3d3c3a',
+  header: {
+    textAlign: 'center',
+    color: 'white',
+    fontSize: 15,
+    borderBottomWidth: 2,
+    backgroundColor: 'black',
+    marginBottom: 5,
+  },
+  nutrient: {
+    textAlign: 'center',
+    color: 'white',
+  },
+  navigator: {
+    activeColor: '#1111',
+    backgroundColor: '#b5b2aa',
+  },
   customFood: {
     width: 40,
     left: 10,
@@ -13,6 +30,7 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     borderColor: 'grey',
     elevation: 1,
+    color: 'white',
   },
 });
 
