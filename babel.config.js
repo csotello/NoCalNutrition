c197528ccbdf57d1b36699791ded3ef26283eb09
Carlos Sotello <59632116+csotello@users.noreply.github.com>
2024-03-10 05:36:31 -0700

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,26 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  presets: ['module:@react-native/babel-preset'],
+  // presets: ['module:@react-native/babel-preset', '@babel/preset-typescript'],
+  presets: [
+    'module:metro-react-native-babel-preset',
+    [
+      '@babel/preset-env',
+      {
+        targets: {node: 'current'},
+        loose: true,
+        shippedProposals: true,
+      },
+    ],
+    // 'module:@react-native/babel-preset',
+    '@babel/preset-typescript',
+  ],
+  plugins: [
+    '@babel/plugin-transform-private-methods',
+    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-proposal-private-methods',
+  ],
+  // plugins: ['@babel/plugin-transform-modules-commonjs'],
 };
+// module.exports = {
+//   presets: ['@babel/preset-env', '@babel/preset-typescript'],
+// };
