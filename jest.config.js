b3267c85a9d58758c11e0757c3a9ebe23ae6c5f9
Carlos Sotello <59632116+csotello@users.noreply.github.com>
2026-09-15 12:03:07 -0700

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,6 @@
 module.exports = {
   preset: '@react-native/jest-preset',
+  transformIgnorePatterns: [
+    './node_modules/(?!(jest-)?@gluestack-ui|react-native|@react-native|@gluestack-ui/themed/*|@gluestack-style)/)'
+  ],
 };
